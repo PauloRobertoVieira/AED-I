fe017a72bd94755f9e11c607ebc000117f07a033
Carlos Eduardo <carlos-eduardo-dev@hotmail.com>
2021-03-20 08:36:16 -0300

    feat(VoiceHandleButton): #1 cria botão para sintetizar voz

---
+++ src/pages/Word/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import VoiceButton from '../../components/VoiceButton';
+import { VoiceHandlerProvider } from '../../contexts/VoiceHandlerContext';
+
+import { Container } from './styles';
+
+export default function Word() {
+    return (
+        <VoiceHandlerProvider>
+            <Container>
+                <VoiceButton />
+            </Container>
+        </VoiceHandlerProvider>
+    );
+}
